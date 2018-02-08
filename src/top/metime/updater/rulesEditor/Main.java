26ae31982ce8a65ea804a91ece62e771efd83da7
innc-table <1805795356@qq.com>
2018-02-08 17:59:24 +0800

    删除影响阅读的注释

--- src/top/metime/updater/rulesEditor/Main.java
+++ src/top/metime/updater/rulesEditor/Main.java
@@ -1,25 +1,22 @@
 package top.metime.updater.rulesEditor;
 
+import top.metime.updater.rulesEditor.view.MainPanel;
+
+/**
+ * 程序的入口类
+ * @author innc
+ */
 public class Main
 {
+	public static final String RULE_FILE_NAME = "serverRules.json";
+	
+	/**
+	 * 程序的入口方法
+	 * @param args Class参数
+	 */
 	public static void main(String[] args)
 	{
-//		new MainWindow(null).init();
-//		new RuleEditPanel().init(null, new HashSet<String>(), "", "");
+		//从一个主面板开始
 		new MainPanel().init();
-//		new InputTextPanel().init(new InputTextPanelEventCallback()
-//		{
-//			@Override
-//			public void onEnter(String textFieldContent)
-//			{
-//				System.out.println("onEnter() "+textFieldContent);
-//			}
-//
-//			@Override
-//			public void onExit()
-//			{
-//				System.out.println("Exit");
-//			}
-//		}, "AG");
 	}
 }
