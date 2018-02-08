189901750916c092346482b5fd6499e9362a98e8
innc-table <1805795356@qq.com>
2018-02-08 17:59:43 +0800

    添加JavaDoc

--- src/top/metime/updater/rulesEditor/callback/InputTextPanelEventCallback.java
+++ src/top/metime/updater/rulesEditor/callback/InputTextPanelEventCallback.java
@@ -1,7 +1,22 @@
 package top.metime.updater.rulesEditor.callback;
 
+/**
+ * 文字编辑面板事件的回调
+ * 
+ * @author innc
+ */
 public interface InputTextPanelEventCallback
 {
+
+	/**
+	 * 当点击文字编辑面板的确定按钮或者输入回车时被调用
+	 * @param textFieldContent 文字内容
+	 * @param tag 标签
+	 */
 	public void onInputTextPanelReturn(String textFieldContent, int tag);
+
+	/**
+	 * 但关闭文字编辑面板时被调用
+	 */
 	public void onInputTextPanelExit();
 }
