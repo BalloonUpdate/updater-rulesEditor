613c610f3fae8647e214181e994c9047e9c643fd
innc-table <1805795356@qq.com>
2018-02-08 18:00:18 +0800

    添加JavaDoc和删除一些多余的方法

--- src/top/metime/updater/rulesEditor/callback/MainWindowEventCallback.java
+++ src/top/metime/updater/rulesEditor/callback/MainWindowEventCallback.java
@@ -1,15 +1,35 @@
 package top.metime.updater.rulesEditor.callback;
 
+/**
+ * 主面板事件的回调接口
+ * @author innc
+ */
 public interface MainWindowEventCallback 
 {
+
+	/**
+	 * 当点击主面板的加载文件按钮时被调用
+	 */
 	public void onClickLoadFileButton();
+
+	/**
+	 * 当点击主面板的保存文件按钮时被调用
+	 */
 	public void onClickSaveFileButton();
 	
+	/**
+	 * 当点击主面板的添加规则按钮时被调用
+	 */
 	public void onClickRuleAdd();
+
+	/**
+	 * 当点击主面板的重命名规则按钮时被调用
+	 */
 	public void onClickRuleRename();
+
+	/**
+	 * 当点击主面板的删除规则按钮时被调用
+	 */
 	public void onClickRuleRemove();
 	
-	public void onClickIgnoreFilesAdd();
-	public void onClickIgnoreFilesEdit();
-	public void onClickIgnoreFilesRemove();
 }
